40cf040ee512f8c9ad3054bcb0688950390a702f
julianNinoo <julian.nino@correo.icesi.edu.co>
2021-04-04 15:30:59 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -13,7 +13,7 @@ import models
 #Index
 @app.route("/")
 def index():
-    return "This is the app indexjulian"
+    return "This is the app indexjulian123"
 
 #Add client and money
 @app.route("/add")
