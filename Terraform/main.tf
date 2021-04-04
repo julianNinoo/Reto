4528e35113310fa38471912fe138f99b695d50be
julianNinoo <julian.nino@correo.icesi.edu.co>
2021-04-04 01:04:37 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,28 +2,28 @@ provider "google" {
   project = "prueba-020421"
   credentials = file("prueba-020421-92334dc99723.json")
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = "us-central1-a"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+  name         = "addi3"
+  machine_type = "e2-small"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
   network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
+ #    A default network is created for all GCP projects
+   network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  name                    = "terraform"
   auto_create_subnetworks = "true"
 }
